88ac1ea590fe206cc08619f6f4c60a822efd18d3
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-07-27 18:29:34 +0530

    feat: sidebar layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import SidebarLayout from "./layouts/SidebarLayout";
+
+import './css/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<SidebarLayout title="Hello world">Hey</SidebarLayout>);
