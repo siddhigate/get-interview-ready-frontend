7d002c9551d82783b9877a1d5c7b0615c3910d58
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-07-27 19:19:00 +0530

    feat: tab component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SidebarLayout from "./layouts/SidebarLayout";
 
 import "./css/style.css";
-import GridContainer from "./components/GridContainer";
+
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <SidebarLayout title="Hello world">
-    <GridContainer>
-      <div>One</div>
-      <div>One</div>
-      <div>One</div>
-      <div>One</div>
-      <div>One</div>
-      <div>One</div>
-      <div>One</div>
-    </GridContainer>
-  </SidebarLayout>
+  <App></App>
 );
