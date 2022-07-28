b0806a3326f3771da1f2118ec27c547f545cfe1e
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-07-28 16:11:51 +0530

    feat: card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import Pill from "./Pill";
+
+const Card = ({ date, title, pills }) => {
+  return (
+    <div className="card card-with-shadow question-card">
+      <p className="date">{date}</p>
+      <div className="question">
+        <span className="dot bg-violet"></span>
+        <p>{title}</p>
+      </div>
+      <div className="tech-pills">
+        {pills.map((pill) => (
+          <Pill text={pill.text} color={pill.color}></Pill>
+        ))}
+      </div>
+      <i className="fa-solid fa-ellipsis-vertical ellipsis-icon question-options"></i>
+    </div>
+  );
+};
+
+export default Card;
