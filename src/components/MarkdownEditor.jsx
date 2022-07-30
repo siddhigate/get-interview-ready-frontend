2c499e7c7a3e8a87aed3e3cf8b8ee97f80c7498f
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-07-30 22:21:24 +0530

    fix: markdown editor

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -3,9 +3,17 @@ import MdEditor from "react-markdown-editor-lite";
 import "react-markdown-editor-lite/lib/index.css";
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
-const MarkdownEditor = ({height, defaultValue, onChange}) => {
+const MarkdownEditor = ({ height = "70vh", defaultValue, onChange }) => {
   return (
     <MdEditor
+      view={{ menu: true, md: true, html: false }}
+      canView={{
+        menu: true,
+        md: true,
+        html: true,
+        fullScreen: true,
+        hideMenu: true,
+      }}
       style={{ height: height }}
       renderHTML={(text) => mdParser.render(text)}
       defaultValue={defaultValue}
