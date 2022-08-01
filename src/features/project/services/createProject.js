59c7ed956edb499d577d7b4efd1c0ea3408b4f4d
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-08-01 08:12:09 +0530

    fix: create insert tech stack

--- src/features/project/services/createProject.js
+++ src/features/project/services/createProject.js
@@ -2,12 +2,15 @@ import axios from "axios";
 import { API_URL } from "../../../constants/api";
 
 export const createProject = async ({ name, tagline, tech_stack }) => {
+
+  const techStr = tech_stack.replaceAll(" ", ",");
+
   return await axios.post(
     `${API_URL}/projects/createProject`,
     {
       name,
       tagline,
-      tech_stack,
+      tech_stack: techStr,
       user_id: JSON.parse(localStorage.getItem("user")).id,
     },
     {
--- src/features/technical/services/createTechnicalQuestion.js
+++ src/features/technical/services/createTechnicalQuestion.js
@@ -2,11 +2,12 @@ import axios from "axios";
 import { API_URL } from "../../../constants/api";
 
 export const createTechnicalQuestion = async ({ question, tech_stack }) => {
+  const techStr = tech_stack.replaceAll(" ", ",");
   return await axios.post(
     `${API_URL}/technicalQuestions/createTechnicalQuestion`,
     {
       question,
-      tech_stack,
+      tech_stack: techStr,
       user_id: JSON.parse(localStorage.getItem("user")).id,
     },
     {
