009b7703ff61e182f5a1b69a69daf690f063df3f
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2023-01-30 17:39:02 +0530

    port backend to render.com

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://get-interview-ready-backend.herokuapp.com/api/v1";
+// export const API_URL = "https://get-interview-ready-backend.herokuapp.com/api/v1";
+export const API_URL = "https://get-interview-ready-backend.onrender.com/api/v1";
